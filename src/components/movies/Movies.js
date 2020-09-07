e05ac1d097edaf5c945f01e3fa6aefaecaede4ae
Tejiri Asah <teasah@hotmail.co.uk>
2020-09-07 15:29:58 -0400

    change className to BEM

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -4,11 +4,9 @@ import "./movies.scss";
 const Movies = (props) => {
   return (
     <>
-      <div className="movie-box">
-        <div className="tester">
-          <img src={props.poster} alt="movie-poster" className="movie-poster" />
-        </div>
-        <div className="movie-info">
+      <div className="movie__box">
+        <img src={props.poster} alt="movie-poster" className="movie__poster" />
+        <div className="movie__info">
           <span className="nomination-item">
             {props.title + " (" + props.year + ")"}
           </span>
