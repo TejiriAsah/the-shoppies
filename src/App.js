35be3a9c424b02e9500f80309155d4403a7b61ca
Tejiri Asah <teasah@hotmail.co.uk>
2020-09-07 17:21:40 -0400

    remove commented out code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from "react";
 import "./App.css";
-// import SearchBar from "./components/searchBar/SearchBar";
-// import SearchResults from "./components/searchResults/SearchResults";
-// import Nominations from "./components/nominations/Nominations";
 import HomePage from "./HomePage";
 
 function App() {
